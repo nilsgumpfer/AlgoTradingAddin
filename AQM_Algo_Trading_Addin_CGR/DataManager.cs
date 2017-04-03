ea794f5f1c6f9709ed02f3eb74e4ef28c3be6393
Nils Gumpfer <nilsgumpfer94@arcor.de>
2017-04-03 17:09:03 +0200

    fix

--- AQM_Algo_Trading_Addin_CGR/DataManager.cs
+++ AQM_Algo_Trading_Addin_CGR/DataManager.cs
@@ -79,19 +79,7 @@ namespace AQM_Algo_Trading_Addin_CGR
             //create worker only in case of new symbol
             if(worker == null)
             {
-<<<<<<< HEAD
                 worker = new PushWorker(variant, symbol);                                 
-=======
-                //**********************************************!!!TESTING!!!*****************************************************
-                //****************************************************************************************************************
-
-                worker = new PushWorker(LiveConnectors.OnVistaDummy, symbol); //TODO: This is just for development and test-usage!
-                //worker = new PushWorker(LiveConnectors.OnVista, symbol);
-
-                //****************************************************************************************************************
-                //**********************************************!!!TESTING!!!*****************************************************
-
->>>>>>> master
                 DBUpdater dbUpdater = new DBUpdater();
 
                 //stash objects for later use
